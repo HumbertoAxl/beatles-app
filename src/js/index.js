fcd83fa2d33a50ef579dd6df58267526d196f683
Humberto Axl <humbertoaxl@gmail.com>
2022-09-27 23:42:41 +0800

    Setando arquivos

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+//Executa os comandos assim que a página e suas dependências (como arquivos js e css) se iniciarem.
+//não é obrigatório mas torna a leitura mais simples além de assegurar que esse script só vai funcionar
+//na hora certa.
+window.onload = () => {
+    console.log("Ba noite seu Nonoca, bem vindo ao debug do Chrome, aqui você pode testar umas coisinhas básicas/checar se funções tão devolvendo valores certos, etc...")
+}
